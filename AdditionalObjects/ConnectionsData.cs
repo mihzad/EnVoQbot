2dfe8f653eae639c94b65be651f4e19284b22b96
Misha Zadorozhnij <117081745+mihzad@users.noreply.github.com>
2024-09-21 13:26:19 +0300

    Update ConnectionsData.cs - deleted bot token

--- AdditionalObjects/ConnectionsData.cs
+++ AdditionalObjects/ConnectionsData.cs
@@ -10,7 +10,7 @@ namespace EnVoQbot.AdditionalObjects
     {
         internal static readonly string DBconnectionString = "Server=DESKTOP-E2LHVUJ;Database=envoqbot-db;Trusted_Connection=True;Encrypt=False;";
         internal static readonly string QuartzDBconnectionString = "Server=DESKTOP-E2LHVUJ;Database=envoqbot-quartz-db;Trusted_Connection=True;Encrypt=False;";
-        internal static readonly string BotConnectionToken = "6013982952:AAGmW38dQiANRZ27gj2MuDcPM9qOXAX98Ig";
+        internal static readonly string BotConnectionToken = "";
     }
 
 
